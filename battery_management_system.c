61ed56d702441a746ec7d02384e1a30f73e01790
RohithEBD2 <rohithkumar7996@gmail.com>
2024-07-24 14:45:31 +0530

    Update battery_management_system.c

--- battery_management_system.c
+++ battery_management_system.c
@@ -5,79 +5,21 @@ void logMessage(const char* message, float value, bool isHigh) {
     printf("%s: %.2f (%s)\n", message, value, isHigh ? "high" : "low");
 }
 
-bool isTemperatureInRange(float temperature) {
-    if (temperature < MIN_TEMPERATURE || temperature > MAX_TEMPERATURE) {
-        logMessage("Temperature out of range", temperature, temperature > MAX_TEMPERATURE);
-        return false;
-    }
-    return true;
-}
-
-void checkHighTemperatureWarning(float temperature) {
-    if (temperature > MAX_TEMPERATURE - TEMPERATURE_WARNING_TOLERANCE && temperature <= MAX_TEMPERATURE) {
-        logMessage("Warning: Approaching high temperature", temperature, true);
-    }
-}
-
-void checkLowTemperatureWarning(float temperature) {
-    if (temperature >= MIN_TEMPERATURE && temperature < MIN_TEMPERATURE + TEMPERATURE_WARNING_TOLERANCE) {
-        logMessage("Warning: Approaching low temperature", temperature, false);
-    }
-}
-
-void checkTemperatureWarning(float temperature) {
-    checkHighTemperatureWarning(temperature);
-    checkLowTemperatureWarning(temperature);
-}
-
-bool isSocInRange(float soc) {
-    if (soc < MIN_SOC || soc > MAX_SOC) {
-        logMessage("State of Charge out of range", soc, soc > MAX_SOC);
-        return false;
-    }
-    return true;
-}
-
-void checkHighSocWarning(float soc) {
-    if (soc > MAX_SOC - SOC_WARNING_TOLERANCE && soc <= MAX_SOC) {
-        logMessage("Warning: Approaching charge-peak", soc, true);
-    }
-}
-
-void checkLowSocWarning(float soc) {
-    if (soc >= MIN_SOC && soc < MIN_SOC + SOC_WARNING_TOLERANCE) {
-        logMessage("Warning: Approaching discharge", soc, false);
-    }
-}
-
-void checkSocWarning(float soc) {
-    checkHighSocWarning(soc);
-    checkLowSocWarning(soc);
-}
-
-bool isChargeRateInRange(float chargeRate) {
-    if (chargeRate > MAX_CHARGE_RATE) {
-        logMessage("Charge Rate out of range", chargeRate, true);
-        return false;
-    }
-    return true;
-}
-
-void checkChargeRateWarning(float chargeRate) {
-    if (chargeRate > MAX_CHARGE_RATE - CHARGE_RATE_WARNING_TOLERANCE && chargeRate <= MAX_CHARGE_RATE) {
-        logMessage("Warning: Approaching high charge rate", chargeRate, true);
-    }
-}
-
-bool batteryIsOk(float temperature, float soc, float chargeRate) {
+bool batteryIsOk(float temperature, float soc, float chargeRate, const WarningConfig* config) {
     bool isOk = true;
     isOk &= isTemperatureInRange(temperature);
     isOk &= isSocInRange(soc);
     isOk &= isChargeRateInRange(chargeRate);
-    
-    checkTemperatureWarning(temperature);
-    checkSocWarning(soc);
-    checkChargeRateWarning(chargeRate);
-    
+
+    if (config->enableTemperatureWarning) {
+        checkTemperatureWarning(temperature, config);
+    }
+    if (config->enableSocWarning) {
+        checkSocWarning(soc, config);
+    }
+    if (config->enableChargeRateWarning) {
+        checkChargeRateWarning(chargeRate, config);
+    }
+
     return isOk;
 }
