50e334a7e6dd10931bd691e17f217cefa688a7b0
RohithEBD2 <rohithkumar7996@gmail.com>
2024-07-24 14:34:55 +0530

    Create Temperature.c

---
+++ Temperature.c
@@ -0,0 +1,31 @@
+#include "battery_management_system.h"
+
+bool isTemperatureInRange(float temperature) {
+    bool isLow = temperature < MIN_TEMPERATURE;
+    bool isHigh = temperature > MAX_TEMPERATURE;
+
+    if (isLow || isHigh) {
+        logMessage("Temperature out of range", temperature, isHigh);
+        return false;
+    }
+    return true;
+}
+
+void checkHighTemperatureWarning(float temperature) {
+    bool isApproachingHigh = temperature > MAX_TEMPERATURE - TEMPERATURE_WARNING_TOLERANCE && temperature <= MAX_TEMPERATURE;
+    if (isApproachingHigh) {
+        logMessage("Warning: Approaching high temperature", temperature, true);
+    }
+}
+
+void checkLowTemperatureWarning(float temperature) {
+    bool isApproachingLow = temperature >= MIN_TEMPERATURE && temperature < MIN_TEMPERATURE + TEMPERATURE_WARNING_TOLERANCE;
+    if (isApproachingLow) {
+        logMessage("Warning: Approaching low temperature", temperature, false);
+    }
+}
+
+void checkTemperatureWarning(float temperature, const WarningConfig* config) {
+    checkHighTemperatureWarning(temperature);
+    checkLowTemperatureWarning(temperature);
+}
