7997fe66c734523653ba74983298ece848ada5b8
RohithEBD2 <rohithkumar7996@gmail.com>
2024-07-24 14:40:01 +0530

    Update Charge_Rate.c

--- Charge_Rate.c
+++ Charge_Rate.c
@@ -1,1 +1,18 @@
+#include "battery_management_system.h"
 
+bool isChargeRateInRange(float chargeRate) {
+    bool isHigh = chargeRate > MAX_CHARGE_RATE;
+
+    if (isHigh) {
+        logMessage("Charge Rate out of range", chargeRate, true);
+        return false;
+    }
+    return true;
+}
+
+void checkChargeRateWarning(float chargeRate, const WarningConfig* config) {
+    bool isApproachingHigh = chargeRate > MAX_CHARGE_RATE - CHARGE_RATE_WARNING_TOLERANCE && chargeRate <= MAX_CHARGE_RATE;
+    if (isApproachingHigh) {
+        logMessage("Warning: Approaching high charge rate", chargeRate, true);
+    }
+}
