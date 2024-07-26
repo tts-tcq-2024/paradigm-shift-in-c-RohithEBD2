4aa1722a2f9930146a445982c8ed0c9c7a462c10
RohithEBD2 <rohithkumar7996@gmail.com>
2024-07-26 11:14:56 +0530

    Update battery_temperature.h

--- battery_temperature.h
+++ battery_temperature.h
@@ -1,1 +1,8 @@
+#include <stdbool.h>
 
+#define MIN_TEMPERATURE 0.0f
+#define MAX_TEMPERATURE 45.0f
+#define TEMPERATURE_WARNING_TOLERANCE 2.25f // 5% of 45
+
+bool isTemperatureInRange(float temperature);
+void checkTemperatureWarning(float temperature, const WarningConfig* config);
