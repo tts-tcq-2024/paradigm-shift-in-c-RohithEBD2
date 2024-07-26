48f16c965260ae2ae5c095e031d8eefb6fc67c07
RohithEBD2 <rohithkumar7996@gmail.com>
2024-07-26 11:18:16 +0530

    Update battery_management_system.h

--- battery_management_system.h
+++ battery_management_system.h
@@ -1,9 +1,5 @@
 #include <stdbool.h>
 
-#define MIN_SOC 20.0f
-#define MAX_SOC 80.0f
-#define SOC_WARNING_TOLERANCE 4.0f // 5% of 80
-
 #define MAX_CHARGE_RATE 0.8f
 #define CHARGE_RATE_WARNING_TOLERANCE 0.04f // 5% of 0.8
 
@@ -14,8 +10,7 @@ typedef struct {
 } WarningConfig;
 
 
-bool isSocInRange(float soc);
-void checkSocWarning(float soc, const WarningConfig* config);
+
 
 bool isChargeRateInRange(float chargeRate);
 void checkChargeRateWarning(float chargeRate, const WarningConfig* config);
@@ -24,7 +19,7 @@ void logMessage(const char* message, float value, bool isHigh);
 bool batteryIsOk(float temperature, float soc, float chargeRate, const WarningConfig* config);
 
 void checkWarnings(float temperature, float soc, float chargeRate, const WarningConfig* config);
-void checkSocWarningIfEnabled(float soc, const WarningConfig* config);
+
 void checkChargeRateWarningIfEnabled(float chargeRate, const WarningConfig* config);
 
 
