3d8048d51e953147ae8c217bb09f64864af24eaa
RohithEBD2 <rohithkumar7996@gmail.com>
2024-07-26 11:22:40 +0530

    Create battery_charge_rate.h

---
+++ battery_charge_rate.h
@@ -0,0 +1,6 @@
+#define MAX_CHARGE_RATE 0.8f
+#define CHARGE_RATE_WARNING_TOLERANCE 0.04f // 5% of 0.8
+
+bool isChargeRateInRange(float chargeRate);
+void checkChargeRateWarning(float chargeRate, const WarningConfig* config);
+void checkChargeRateWarningIfEnabled(float chargeRate, const WarningConfig* config);
