7ae84e05bf6a9373d0cd27b55e6a119d017def4e
RohithEBD2 <rohithkumar7996@gmail.com>
2024-07-26 11:19:30 +0530

    Update battery_state_of_charge.h

--- battery_state_of_charge.h
+++ battery_state_of_charge.h
@@ -1,1 +1,9 @@
+#include <stdbool.h>
 
+#define MIN_SOC 20.0f
+#define MAX_SOC 80.0f
+#define SOC_WARNING_TOLERANCE 4.0f // 5% of 80
+
+bool isSocInRange(float soc);
+void checkSocWarning(float soc, const WarningConfig* config);
+void checkSocWarningIfEnabled(float soc, const WarningConfig* config);
